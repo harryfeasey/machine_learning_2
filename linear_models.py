75b803ae23e636cbac0f0fec629a0fb0ee27a917
Harry Feasey <hef25@uclive.ac.nz>
2020-09-22 16:30:45 +1200

    yeah cool question 3 and 4 done

--- linear_models.py
+++ linear_models.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 def dot(x, y):
@@ -18,14 +19,44 @@ def linear_regression_2d(data):
         x.append(feature)
         y.append(response)
 
-
     n = len(x)
     m = (n * dot(x, y) - (sum(x) * sum(y))) / (n * dot(x, x) - (sum(x) ** 2))
     c = (sum(y) - m*(sum(x))) / n
     return m, c
 
 
-data = [(1, 4), (2, 7), (3, 10)]
-m, c = linear_regression_2d(data)
-print(m, c)
-print(4 * m + c)
+def linear_regression_(x, y, penalty=None):
+    # (X.T * X + λI)^-1 X.T * y
+    x_t_x = np.matmul(x.T, x)
+    if penalty:
+        identity = penalty * np.identity(x_t_x.shape[0])
+        x_t_x = np.add(x_t_x, identity)
+    inverted = np.linalg.inv(x_t_x)
+    x_t_y = np.matmul(x.T, y)
+
+    return np.matmul(inverted, x_t_y)
+
+
+def linear_regression(xs, ys, basis_functions=None, penalty=0):
+
+    if basis_functions or basis_functions == []:
+        new_x = np.ones(xs.shape[0]).reshape(-1, 1)
+        for function in basis_functions:
+            x = [function(x) for x in xs]
+            new_x = np.insert(new_x, new_x.shape[1], x, axis=1)
+
+        return linear_regression_(new_x, ys, penalty)
+    else:
+        xs = np.insert(xs, 0, np.ones(xs.shape[0]), axis=1)
+        return linear_regression_(xs, ys, penalty)
+
+
+import numpy as np
+
+xs = np.arange(5).reshape((-1, 1))
+ys = np.arange(1, 11, 2)
+
+print(linear_regression(xs, ys), end="\n\n")
+
+with np.printoptions(precision=5, suppress=True):
+    print(linear_regression(xs, ys, penalty=0.1))
